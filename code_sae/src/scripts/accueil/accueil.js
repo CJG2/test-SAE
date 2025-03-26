828ac0956844af268a5e8ef5db731c4f5d040bce
Guillermo <carlos.GUILLERMO-GOMEZ@etu.univ-amu.fr>
2025-03-26 22:52:20 +0100

    Modifs des chemins 3

--- code_sae/src/scripts/accueil/accueil.js
+++ code_sae/src/scripts/accueil/accueil.js
@@ -77,7 +77,7 @@ document.addEventListener("DOMContentLoaded", () => {
     createNavLink(
       "accueilLinkImagesApprendre",
       "Apprends à lire et à écrire avec des exercices !",
-      "/test-SAE/code_sae/dist/apprendre.html"
+      "/apprendre.html"
     )
   );
 
@@ -85,7 +85,7 @@ document.addEventListener("DOMContentLoaded", () => {
     createNavLink(
       "accueilLinkImagesMiniGames",
       "Apprends en t'amusant avec des exercices !",
-      "/test-SAE/code_sae/dist/miniJeux.html"
+      "/miniJeux.html"
     )
   );
 
